76ea07c1c6bd06b8563080451f048576ac2c1a0b
Eugene Golovanov <eugene.golovanov@hart.com>
2016-08-04 18:23:44 -0700

    middleware user verification added

--- middleware.js
+++ middleware.js
@@ -1,5 +1,8 @@
 var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./config'); // get our config file
+var User = require('./models/user.js');
+var Token = require('./models/token.js');
+var cryptojs = require('crypto-js');
 
  
 var middleware = {
@@ -7,22 +10,45 @@ var middleware = {
         // check header or url parameters or post parameters for token
         var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
-        // decode token
         if (token) {
 
-            // verifies secret and checks exp
-            jwt.verify(token, config.jwt, function(err, decoded) {      
+		    var tokenHash = cryptojs.MD5(token).toString();
+
+            //Search token in db
+            Token.findOne({'tokenHash' : tokenHash}, function(err, todo) {
                 if (err) {
-                    return res.json({ success: false, message: 'Failed to authenticate token.' });    
+                    return res.status(404).json({"token":"Not Found"});//500 status - server error
                 } else {
-                    // if everything is good, save to request for use in other routes
-                    req.decoded = decoded;    
-                    next();
+                    //TOKEN FOUND
+                    //Decode back jwt
+                    var tokenDecodedJWT = jwt.verify(token, config.jwt);// encoding encoded token
+                    var bytes = cryptojs.AES.decrypt(tokenDecodedJWT.token, config.cryptojs);
+                    var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));//{id: user.id}
+
+                    var userId = tokenData.id;//USER ID FROM TOKEN
+
+                    //Search User By extracted Id
+                    User.findOne({"_id": userId}, function(err, user) {
+                            if(err){
+                                return res.status(401).json({"user":"not exists"});
+                            }
+                        if (!user) {
+                            res.json({ success: false, message: 'Authentication failed. User not found.' });
+                        } else if (user) {
+                            //SUCCESS
+                             console.log('++++++++User Authorized+++++++++');
+                             next();
+                        }
+                    });
+
+
+
+
                 }
+
+
             });
-
         } else {
-
             // if there is no token
             // return an error
             return res.status(403).send({ 
